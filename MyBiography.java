c7d7e30640792831a3e8491202fb2ce2683abaa2
Ahmed <a.masriiee@gmail.com>
2022-07-29 06:37:22 +0200

    this is the first java file that i made

---
+++ MyBiography.java
@@ -0,0 +1,16 @@
+public class MyBiography {
+    public static void main(String[] args) {
+        String name = "Ahmed";
+        var age = 30;
+        String country = "Germany";
+        String sport = "Football";
+        var hours = 5;
+        String game = "playstation";
+        String subject = "physics";
+        char grade = 'A';
+        System.out.println("My name is " + name + " I am " + age + " and i am from " + country);
+        System.out.println("My favourite sport is " + sport +  " i play for " + hours + " hours a day");
+        System.out.println("when i am tired i like to play " + game);
+        System.out.println("In school my favourite subject was " + subject + " i scored a/an " + grade);   
+    }
+}
